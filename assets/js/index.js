6c8e41ada35303936b99c4870e992352a512bf0f
celmil <celmil>
2022-10-05 16:55:05 +0200

    Added slideshow to portfolio, removed unused files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,30 @@
-import { getWeather } from "./getWebInfo.js";
+let slideIndex = 1;
+showSlides(slideIndex);
 
-const weatherText = document.getElementById("weatherInfo");
-
-
-
-
-main(); 
-
-function main(){
-    //getResults().then(insertResults);
-    getWeather().then(logWeather);
-    
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function logWeather(weather){
-    weatherText.innerHTML = `Den kommande timmen är det ${weather}C grader i Djupvik.`;
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
